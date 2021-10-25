2d07c923f565886b00103447aae7f156a11808cb
Zach Shelton <zachshelton2000@gmail.com>
2021-10-24 23:49:53 -0500

    Well first attempt

--- Testing.py
+++ Testing.py
@@ -1,1 +1,367 @@
+valor = {
+    'item' : 'Breastplate of Valor',
+    'physical prots' : 65,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : 300,
+    'mp5' : 10,
+    'cooldown' : .2,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : False,
+    'magical power' : False, 
+    'passive' : False,
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : False
+    }
+
+genjis = {
+    'item' : "Genji's Guard",
+    'physical prots' : False,
+    'magical prots' : 70,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : 40,
+    'cooldown' : .1,
+    'hp5' : False,
+    'health' : 150,
+    'movement' : False,
+    'physical power' : False,
+    'magical power' : False, 
+    'passive' : 'When you take Magical Damage from Abilities your cooldowns are reduced by 3s. This can only occur once every 30s.',
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : False
+    }
+
+bulwark = {
+    'item' : 'Bulwark of Hope',
+    'physical prots' : False,
+    'magical prots' : 80,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : False,
+    'hp5' : False,
+    'health' : 250,
+    'movement' : False,
+    'physical power' : False,
+    'magical power' : False, 
+    'passive' : 'When you take damage and are below 40% Health, you gain a Shield with health equal to 15 percent of your Maximum Health for 20s. Can only occur once every 60s.',
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : .2
+    }
+failnot = {
+    'item' : 'Fail-not',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : .2,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : 40,
+    'magical power' : False, 
+    'passive' : 'When your Ultimate ability has finished casting, your next ability or basic attack within 8s that damages an enemy god marks them, increasing the chance you and your allies can land a Critical Strike by 20 percent for 10 seconds. This can only occur once every 45 seconds.',
+    'attack speed' : False,
+    'percent pen' : .1,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : .1,
+    'CCR' : False
+    }
+soul = {
+    'item' : 'Soul Gem',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : .1,
+    'hp5' : False,
+    'health' : 150,
+    'movement' : False,
+    'physical power' : False,
+    'magical power' : 80, 
+    'passive' : 'On successful hit of an Ability you gain 1 stack. At 4 Stacks your next Ability that damages an enemy God will deal bonus damage equal to 25% of your Magical power to each God hit, and will heal yourself and allies within 20 units for 30 percent of your Magical Power and will consume the 4 stacks.',
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : .12,
+    'crit' : False,
+    'CCR' : False
+    }
+jotunn = {
+    'item' : 'Jotunn\'s Wrath',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : 150,
+    'mp5' : False,
+    'cooldown' : .2,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : 45,
+    'magical power' : False, 
+    'passive' : False,
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : 10,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : False
+    }
+hyrdras = {
+    'item' : 'Hydra\'s Lament',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : 10,
+    'cooldown' : .1,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : 40,
+    'magical power' : False, 
+    'passive' : 'For 8s after using an ability, your next Basic Attack will deal an additional 40 percent damage. Abilities that function like basic attacks do not benefit from this. PASSIVE - This item grants 2.5 MP5 per 10 percent of your missing Mana.',
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : False
+    }
+malice = {
+    'item' : 'Malice',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : .2,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : 40,
+    'magical power' : False, 
+    'passive' : 'Successfully Hitting an Enemy God with a Critical Strike will subtract 3s from all of your abilities currently on Cooldown, except your Ultimate ability. This effect can only happen once every 5s.',
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : .25,
+    'CCR' : False
+    }
+soul_eater = {
+    'item' : 'Soul Eater',
+    'physical prots' : False,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : .1,
+    'hp5' : False,
+    'health' : False,
+    'movement' : False,
+    'physical power' : 35,
+    'magical power' : False, 
+    'passive' : 'Abilites heal 20 percent of damage done.',
+    'attack speed' : False,
+    'percent pen' : .1,
+    'flat pen' : False,
+    'lifesteal' : .1,
+    'crit' : False,
+    'CCR' : False
+    }
+sov = {
+    'item' : 'Sovereignty',
+    'physical prots' : 60,
+    'magical prots' : False,
+    'damage mit' : False,
+    'mana' : False,
+    'mp5' : False,
+    'cooldown' : .2,
+    'hp5' : 35,
+    'health' : 250,
+    'movement' : False,
+    'physical power' : False,
+    'magical power' : False, 
+    'passive' : False,
+    'attack speed' : False,
+    'percent pen' : False,
+    'flat pen' : False,
+    'lifesteal' : False,
+    'crit' : False,
+    'CCR' : False
+    }
+
+def get_item1():
+    item = input('Please input the first item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+def get_item2():
+    item = input('Please input the second item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+def get_item3():
+    item = input('Please input the third item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+def get_item4():
+    item = input('Please input the fourth item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+def get_item5():
+    item = input('Please input the fifth item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+def get_item6():
+    item = input('Please input the sixth item ')
+    if item == 'sov':
+        return sov
+    if item == 'valor':
+        return valor
+    if item == 'genjis':
+        return genjis
+    if item == 'bulwark':
+        return bulwark
+    if item == 'failnot':
+        return failnot
+    if item == 'soul':
+        return soul
+    if item == 'jotunn':
+        return jotunn
+    if item == 'hydras':
+        return hyrdras
+    if item == 'malice':
+        return malice
+    else:
+        print('That item is either not in the database or has a typo')
+
+
+def user_menu():
+    print('Welcome to the Smite Calculator would you please enter the items you would like on your character')
+    print('The items currently listed are valor, genjis, bulwark, failnot, soul, jotunn, hydras, malice, soul_eater, sov')
+    item1 = get_item1()
+    item2 = get_item2()
+    item3 = get_item3()
+    item4 = get_item4()
+    item5 = get_item5()
+    item6 = get_item6()
+    phys_prots = item1['physical prots'] + item2['physical prots'] + item3['physical prots'] + item4['physical prots'] + item5['physical prots'] + item6['physical prots']
+    print(f'The physical protections are {phys_prots}')
+
+if __name__ == '__main__':
+  user_menu()
 
